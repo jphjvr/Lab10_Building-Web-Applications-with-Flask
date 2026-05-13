afe67f0033d12c60d08955d5f8f2f566fa93c1cd
jphjvr <japhetjavier27@gmail.com>
2026-05-13 12:34:45 +0800

    Initial commit

---
+++ models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+
+
+
+db = SQLAlchemy()
+
+def create_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'  # optional but good practice
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+class Student(db.Model):
+    __tablename__ = 'students'  # optional but good practice
+    
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Student {self.full_name}>'
